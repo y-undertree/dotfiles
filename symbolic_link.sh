0baa4636b53406d10b960c9e81b7d12ca60c7cf9
sample <sample@gmail.com>
2014-02-23 12:17:17 +0900

    git complate & git branch disp

--- symbolic_link.sh
+++ symbolic_link.sh
@@ -10,3 +10,10 @@ for filename in ${DOT_FILES}; do
         echo "ln -s $PWD/$filename $HOME/$filename create symbolic link"
     fi
 done
+
+# git complete
+echo "source /usr/share/doc/git-1.7.1/contrib/completion/git-completion.bash" >> ~/.bash_profile
+
+# branch 補完
+GIT_PS1_SHOWDIRTYSTATE=true
+export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[34m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
