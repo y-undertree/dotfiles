19660def43f292dcb34920b4c8f27d962ceeb4b4
y.kinoshita <yasunori.kinoshita@andpad.co.jp>
2024-09-09 09:11:24 +0900

    feat(nvim): changed default theme

--- dot_config/nvim/lua/custom/chadrc.lua
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "nightfox",
-  theme_toggle = { "nightfox", "one_light" },
+  theme = "github_dark",
+  theme_toggle = { "github_dark", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
