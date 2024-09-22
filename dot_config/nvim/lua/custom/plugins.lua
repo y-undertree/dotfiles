b7a9998414180ffe7d24854abfa6ee90e89b242c
y.kinoshita <yasunori.kinoshita@andpad.co.jp>
2024-09-22 21:40:58 +0900

    fix(nvim): change im-select absolute path

--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -541,7 +541,7 @@ local plugins = {
     config = function()
       require("im_select").setup {
         default_im_select = "com.apple.keylayout.ABC",
-        default_command = "im-select",
+        default_command = "/usr/local/bin/im-select",
         set_default_events = { "InsertLeave", "CmdlineLeave" },
         set_previous_events = {},
       }
