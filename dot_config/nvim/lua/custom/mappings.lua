58c6b568a704907c0488d4e6283c27e03820346b
y.kinoshita <yasunori.kinoshita@andpad.co.jp>
2024-11-23 18:39:38 +0900

    feat(nvim): Change the Telescope find files keybind

--- dot_config/nvim/lua/custom/mappings.lua
+++ dot_config/nvim/lua/custom/mappings.lua
@@ -101,11 +101,11 @@ M.search = {
 M.telescope = {
   n = {
     -- find
-    ["<leader>fd"] = { [[:lua require("telescope").extensions.smart_open.smart_open({ cwd_only = true })<CR>]], "find files by smart open" },
+    ["<leader>ff"] = { [[:lua require("telescope").extensions.smart_open.smart_open({ cwd_only = true })<CR>]], "find files by smart open" },
     ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "find all" },
     ["<leader>fg"] = { "<cmd> Telescope live_grep_args <CR>", "live grep" },
     ["gl"] = { [[:lua require("telescope-live-grep-args.shortcuts").grep_word_under_cursor({ postfix='', quote=false })<cr>]], "live grep" },
-    ["<leader>ff"] = { "<cmd> Telescope buffers <CR>", "find buffers" },
+    ["<leader>fd"] = { "<cmd> Telescope buffers <CR>", "find buffers" },
     ["<leader>fo"] = { "<cmd> Telescope oldfiles <CR>", "find oldfiles" },
     ["<leader>fc"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "find in current buffer" },
     ["<leader>fj"] = { "<cmd> Telescope jumplist <CR>", "telescope jumplist" },
