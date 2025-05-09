46b495ba4e6f13a058b0d0d5a2585357717040be
yasunori.kinoshita <yasunori.kinoshita@andpad.co.jp>
2025-05-09 19:08:20 +0900

    feat(nvim): Update the yazi's config

--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -1521,18 +1521,6 @@ local plugins = {
     "tpope/vim-rails",
     lazy = false,
   },
-  ---@type LazySpec
-  {
-    "mikavilpas/yazi.nvim",
-    event = "VeryLazy",
-    ---@type YaziConfig
-    opts = {
-      open_for_directories = false,
-      keymaps = {
-        show_help = '?',
-      },
-    },
-  },
   {
     "monaqa/dial.nvim",
     event = "VeryLazy",
@@ -1768,6 +1756,30 @@ local plugins = {
         },
       })
     end
+  },
+  ---@type LazySpec
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      -- check the installation instructions at
+      -- https://github.com/folke/snacks.nvim
+      "folke/snacks.nvim"
+    },
+    ---@type YaziConfig | {}
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = true,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+    -- 👇 if you use `open_for_directories=true`, this is recommended
+    init = function()
+      -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+      -- vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+    end,
   },
 }
 
