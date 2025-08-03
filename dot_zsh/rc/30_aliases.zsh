0ab9a262fcaaf8f0fd4c7c822af708e3ed30ff69
y.kinoshita <undertree.ys@gmail.com>
2025-08-03 14:54:48 +0900

    feat(zsh): Improve zsh
    - Move to 01_zinit.zsh for a zinit plugin config
    - Add mdumitru/git-aliases
    - Add empresslabs/pnpm.plugin.zsh
    - Add djui/alias-tips
    - Add alias-preview auto load
    - Refactor alias

--- dot_zsh/rc/30_aliases.zsh
+++ dot_zsh/rc/30_aliases.zsh
@@ -1,6 +1,3 @@
-#unalias ctags
-#unalias cp
-#unalias find
 alias vim='nvim'
 alias find='gfind'
 alias sed='gsed'
@@ -8,7 +5,6 @@ alias awsp='export AWS_PROFILE=$(sed -n "s/\[\(profile \)\?\|\]//gp" ~/.aws/conf
 alias ctags="`brew --prefix`/opt/universal-ctags/bin/ctags"
 alias ctags-build="ctags -R -f .tags --fields=+ilnKz"
 alias ll='eza -la'
-# require zsh plugin = supercrabtree/k
 alias tailf='tail -f'
 alias mkdir='mkdir -p'
 alias pp='popd'
@@ -17,16 +13,13 @@ alias killspring="ps aux | grep spring | grep -v grep | awk '{print $2}' | xargs
 alias killpuma="ps aux | grep puma | grep -v grep | awk '{print $2}' | xargs kill -9"
 alias ff=fzf
 alias ffp="fzf --ansi --delimiter : --preview 'bat --color=always {1} --highlight-line {2}'"
-alias h='function hdi(){ howdoi $* -c -n 5; }; hdi'
-alias g='git'
 alias gw='git worktree'
-alias t='tig'
 alias tigs='tig status'
-alias tb='tig blame'
+alias tigb='tig blame'
 alias lzd='lazydocker'
 alias doc='tldr'
 alias yz=yazi
 alias cm=chezmoi
 alias nocolor='sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"'
 alias beep='afplay /System/Library/Sounds/Hero.aiff'
-alias codeghq='code "`ghq root`/`ghq list | fzf`"'
+alias ghql='code "`ghq root`/`ghq list | fzf`"'
